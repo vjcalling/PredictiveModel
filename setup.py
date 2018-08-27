932d715f95a6fdc632e14f62a960976cef3cf5df
Vibhor Jain (vibhjain) <vibhjain@cisco.com>
2018-08-27 21:11:55 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='sample data science project',
+    author='vibhor jain',
+    license='MIT',
+)
